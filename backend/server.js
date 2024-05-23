e16c9e62a39e05096a4ede8af320c2a324ea4621
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-23 11:41:36 -0600

    Added the first  route

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,23 @@ import express from "express";
 import "dotenv/config";
 
 import { mongoConfig } from "./config.js";
-
+import Todo from "./models/TodoModel.js";
 const port = 8080;
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.json("Hello! (from Server)");
+});
+
+app.get("/api/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json;
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json(err);
+  }
 });
 
 app.listen(port, () => {
