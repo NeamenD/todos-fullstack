010859616baa7b9b895561247829b8c61fe60ab1
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-23 14:09:05 -0600

    added a button and input fild in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,36 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import "./App.css";
 
 function App() {
-  return <>Hello! (from client)</>;
+  const textRef = useRef();
+
+  const completeRef = useRef();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(textRef.current.value);
+    console.log(textRef.current.checked);
+  }
+  return (
+    <div>
+      <h1>Todo</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          I want to :
+          <input type="text" ref={textRef} />
+        </label>
+        <label htmlFor="">
+          <input type="checkbox" ref={completeRef} />
+        </label>
+        <div>
+          <br />
+          <button>ADD Todo</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
