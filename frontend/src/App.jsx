53d810102078f9b8340ba34eff56e68a7bbb354f
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-24 10:10:16 -0600

    changed baseURL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,11 @@ import { useState, useRef, useEffect } from "react";
 
 import "./App.css";
 
-const BASE_URL = "http://localhost:8080/api/todos";
+const BASE_URL = import.meta.env.DEV
+  ? "http://localhost:8080/api/todos"
+  : "https://todos-backend-34z8.onrender.com/api/todos";
+
+console.log(import.meta.env);
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
